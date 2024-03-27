0bfb4352e29c1aaea3ced1848e058a4dc4090623
nickumia <nickumia6@gmail.com>
2024-03-26 23:22:02 -0400

    cleanup: for now ignore all errors so that stdout is cleaner

--- server.go
+++ server.go
@@ -1,17 +1,6 @@
 //go:build OMIT
 // +build OMIT
 
-// The server program issues Google search requests and demonstrates the use of
-// the go.net Context API. It serves on port 8080.
-//
-// The /search endpoint accepts these query params:
-//
-//	q=the Google search query
-//	timeout=a timeout for the request, in time.Duration format
-//
-// For example, http://localhost:8080/search?q=golang&timeout=1s serves the
-// first few Google search results for "golang" or a "deadline exceeded" error
-// if the timeout expires.
 package main
 
 import (
@@ -54,7 +43,7 @@ func handleEmail(w http.ResponseWriter, req *http.Request) {
 	var t Email
 	err := decoder.Decode(&t)
 	if err != nil {
-		fmt.Println(err)
+		// log.Println(err)
 	}
 	// TODO: Save to file
 	// saveFile, err := os.OpenFile(replacer.Replace(now.Format("Mon Jan 2 15:04:05 MST 2006")), os.O_APPEND|os.O_CREATE, 0644)
